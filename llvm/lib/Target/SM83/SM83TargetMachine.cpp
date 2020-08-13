1e5473868fa552c41f29747426b5bdd220e583e5
James Larrowe <larrowe.semaj11@gmail.com>
2020-08-13 13:22:55 -0400

    Start smoothing out existing work

--- llvm/lib/Target/SM83/SM83TargetMachine.cpp
+++ llvm/lib/Target/SM83/SM83TargetMachine.cpp
@@ -1,1 +1,53 @@
+//===-- SM83TargetMachine.cpp - Define TargetMachine for SM83 -------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file defines the SM83-specific subclass of TargetMachine.
+//
+//===----------------------------------------------------------------------===//
+
 #include "SM83TargetMachine.h"
+
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/CodeGen/Passes.h"
+#include "llvm/CodeGen/TargetLoweringObjectFileImpl.h"
+#include "llvm/CodeGen/TargetPassConfig.h"
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Support/FormattedStream.h"
+#include "llvm/Support/TargetRegistry.h"
+#include "llvm/Target/TargetOptions.h"
+
+namespace llvm {
+
+extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeSM83Target() {
+  RegisterTargetMachine<SM83TargetMachine> X(getTheSM83Target());
+}
+
+static const char SM83DataLayout[] = "e-p:16:8-i8:8-i16:8-n16-S8";
+
+static Reloc::Model getEffectiveRelocModel(Optional<Reloc::Model> RM) {
+  return RM.hasValue() ? *RM : Reloc::Static;
+}
+
+SM83TargetMachine::SM83TargetMachine(const Target &T, const Triple &TT,
+                                     StringRef CPU, StringRef FS,
+                                     const TargetOptions &Options,
+                                     Optional<Reloc::Model> RM,
+                                     Optional<CodeModel::Model> CM,
+                                     CodeGenOpt::Level OL, bool JIT)
+    : LLVMTargetMachine(T, SM83DataLayout, TT, CPU, FS, Options,
+                        getEffectiveRelocModel(RM),
+                        getEffectiveCodeModel(CM, CodeModel::Small), OL),
+      TLOF(make_unique<SM83TargetObjectFile>()) {
+  initAsmInfo();
+}
+
+TargetPassConfig *SM83TargetMachine::createPassConfig(PassManagerBase &PM) {
+  return new TargetPassConfig(*this, PM);
+}
+
+} // end namespace llvm
--- llvm/lib/Target/SM83/SM83TargetMachine.h
+++ llvm/lib/Target/SM83/SM83TargetMachine.h
@@ -1,10 +1,41 @@
+//===-- SM83TargetMachine.h - Define TargetMachine for SM83 -----*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file declares the SM83-specific subclass of TargetMachine.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_SM83_TARGET_MACHINE_H
+#define LLVM_SM83_TARGET_MACHINE_H
+
+#include "llvm/CodeGen/SelectionDAGTargetInfo.h"
+#include "llvm/IR/DataLayout.h"
 #include "llvm/Target/TargetMachine.h"
 
 namespace llvm {
 
-class SM83TargetMachine final : public LLVMTargetMachine {
+class SM83TargetMachine : public LLVMTargetMachine {
+  std::unique_ptr<TargetLoweringObjectFile> TLOF;
+
 public:
-  SM83TargetMachine(const Target &T, const Triple &TT);
-}
+  SM83TargetMachine(const Target &T, const Triple &TT, StringRef CPU,
+                    StringRef FS, const TargetOptions &Options,
+                    Optional<Reloc::Model> RM,
+                    Optional<CodeModel::Model> CM,
+                    CodeGenOpt::Level OL, bool JIT);
+
+  TargetPassConfig *createPassConfig(PassManagerBase &PM) override;
+
+  TargetLoweringObjectFile *getObjFileLowering() const override {
+    return TLOF.get();
+  }
+};
 
 } // end namespace llvm
+
+#endif // LLVM_SM83_TARGET_MACHINE_H
