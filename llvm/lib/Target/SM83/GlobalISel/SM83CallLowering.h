72ef081c9cdb168552aab73db8527e315001880b
James Larrowe <larrowe.semaj11@gmail.com>
2020-11-10 13:53:53 -0500

    Finish instructions and start work on GlobalISel

---
+++ llvm/lib/Target/SM83/GlobalISel/SM83CallLowering.h
@@ -0,0 +1,30 @@
+#ifndef LLVM_LIB_TARGET_SM83_GLOBALISEL_SM83CALLLOWERING_H
+#define LLVM_LIB_TARGET_SM83_GLOBALISEL_SM83CALLLOWERING_H
+
+#include "SM83ISelLowering.h"
+#include "llvm/CodeGen/CallingConvLower.h"
+#include "llvm/CodeGen/GlobalISel/CallLowering.h"
+#include "llvm/CodeGen/ValueTypes.h"
+
+namespace llvm {
+
+class SM83CallLowering : public CallLowering {
+public:
+  SM83CallLowering(const SM83TargetLowering &TL)
+    : CallLowering(&TL) {}
+
+  bool supportSwiftError() const override { return false; }
+
+  bool lowerReturn(MachineIRBuilder &MIRBuilder, const Value *Val,
+                   ArrayRef<Register> VRegs) const override;
+
+  bool lowerFormalArguments(MachineIRBuilder &MIRBuilder, const Function &F,
+                            ArrayRef<ArrayRef<Register>> VRegs) const override;
+
+  bool lowerCall(MachineIRBuilder &MIRBuilder,
+                 CallLoweringInfo &Info) const override;
+};
+
+}
+
+#endif
