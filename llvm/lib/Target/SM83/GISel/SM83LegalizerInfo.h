cb2bf2ae8e54f87ee9bd94c7e78cd94ea446bd9c
James Larrowe <larrowe.semaj11@gmail.com>
2020-11-28 12:39:19 -0500

    Add register banks

---
+++ llvm/lib/Target/SM83/GISel/SM83LegalizerInfo.h
@@ -0,0 +1,22 @@
+//===- SM83LegalizerInfo -----------------------------------------*- C++ -*-==//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+// This file declares the targeting of the LegalizerInfo class for SM83.
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_LIB_TARGET_SM83_GISEL_SM83LEGALIZERINFO_H
+#define LLVM_LIB_TARGET_SM83_GISEL_SM83LEGALIZERINFO_H
+
+#include "llvm/CodeGen/GlobalISel/LegalizerInfo.h"
+
+namespace llvm {
+
+class SM83LegalizerInfo : public LegalizerInfo {
+public:
+  SM83LegalizerInfo();
+};
+#endif
---
+++ llvm/lib/Target/SM83/GISel/SM83RegisterBankInfo.h
@@ -0,0 +1,41 @@
+//===- SM83RegisterBankInfo --------------------------------------*- C++ -*-==//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_LIB_TARGET_SM83_GISEL_SM83REGISTERBANKINFO_H
+#define LLVM_LIB_TARGET_SM83_GISEL_SM83REGISTERBANKINFO_H
+
+#include "MCTargetDesc/SM83MCTargetDesc.h"
+#include "llvm/CodeGen/GlobalISel/RegisterBankInfo.h"
+
+#define GET_REGBANK_DECLARATIONS
+#include "SM83GenRegisterBank.inc"
+
+namespace llvm {
+
+class SM83GenRegisterBankInfo : public RegisterBankInfo {
+protected:
+#define GET_TARGET_REGBANK_CLASS
+#include "SM83GenRegisterBank.inc"
+};
+
+class SM83RegisterBankInfo final : public SM83GenRegisterBankInfo {
+public:
+  SM83RegisterBankInfo(const TargetRegisterInfo &TRI)
+    : SM83GenRegisterBankInfo() {}
+
+  const RegisterBank &getRegBankFromRegClass(
+      const TargetRegisterClass &RC, LLT Ty) const override;
+
+  const InstructionMapping &getInstrMapping(
+      const MachineInstr &MI) const override;
+
+  InstructionMappings getInstrAlternativeMappings(
+      const MachineInstr &MI) const override;
+};
+}
+#endif
